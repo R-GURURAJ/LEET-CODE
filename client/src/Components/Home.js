d41045c2082b47af9ba8ddcde2c5ce2d4c02fb19
R-GURURAJ <guru1guru0@gmail.com>
2024-06-07 10:18:40 +0530

    add the ui

---
+++ client/src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex bg-slate-500 h-full w-full'>
+          
+
+      <div className="h-full">
+j
+      </div>
+      <div className="">
+        <div className=""></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
